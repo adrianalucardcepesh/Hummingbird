8204cc163e90ac9440d6bfba125159e93e84c6a9
Alucard <149864103+adrianalucardcepesh@users.noreply.github.com>
2024-03-26 13:12:13 +0300

    Update db-config.js

--- database/db-config.js
+++ database/db-config.js
@@ -14,8 +14,8 @@
 const dbConfig = {
     host: '95.164.21.161',
     database: 'smtp_admin_hum',
-    user: 'smtp_admin_hum',
-    password: ')ffOX$oTY9a*}p]s',
+    user: 'user1',
+    password: 'C7ceb1fd&',
 };
 
 module.exports = {
